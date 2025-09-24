e77e7e1b42baa2a131163c0bab2a893b061242bd
Duckaphacode100nam <nguyenduca355@gmail.com>
2025-09-24 23:13:13 +0700

    NestJS project

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,15 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User, UserSchema } from './schemas/user.schema';
-
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     JwtModule.register({
-      secret: 'mySecretKey',
+      secret: process.env.JWT_SECRET || 'secret123',
       signOptions: { expiresIn: '1h' },
     }),
   ],
--- src/auth/schemas/user.schema.ts
+++ src/auth/schemas/user.schema.ts
@@ -13,4 +13,3 @@ export class User {
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
-    
